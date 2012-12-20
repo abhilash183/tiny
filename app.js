253afa132e4ff2f8c156d3b05ca193b05cb589fb
Mallikarjun <arjun@peek.ly>
2012-12-20 08:06:35 -0500

    init commit

---
+++ app.js
@@ -0,0 +1,5 @@
+var tiny = require('./tiny.js');
+
+var tiny_url = tiny.generate_tiny_url('http://www.google.com/helloworld');
+
+console.log(tiny_url);
---
+++ tiny.js
@@ -0,0 +1,14 @@
+
+/**
+ * This method generates the tiny url from the actual url.
+ * Tiny url consists of 6 alphanumeric characters ~= 2billion possibilities
+ * @param actual_url: url to shorten
+ * @return tiny_url: just the url path (domain name should be filled out)
+ */
+exports.generate_tiny_url = function(actual_url){
+  var tiny_url;
+
+  tiny_url = actual_url;
+
+  return tiny_url;
+}
